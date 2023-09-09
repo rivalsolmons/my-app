411aabbca0f80d2647550d0ae6c334f9612a9445
rivalsolmons <rivalsolmons@gmail.com>
2023-09-09 20:51:42 +0530

    Use State hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+// By default useEffect returns empty array 
 function App() {
+
+  //var val = "Sandy";
+
+  const [val, funcX] = useState('My Game');
+  const changeName = () => {
+     //val = "Hello Sir";
+    //  console.log(val);
+      funcX("Nakli Game")
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sandeepak's React App
+          <h3>{val}</h3>
+          <button className="btn" onClick={changeName}> Click Here</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
+      
     </div>
   );
 }
